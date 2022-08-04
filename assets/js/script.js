2bb1ffa48311a88a619c2539cc8c4eb073fcb78e
Will <omonayeg@gmail.com>
2022-08-04 21:01:29 +0100

    updated the carousel for products

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,24 @@ moreArrow.onclick = function() {
  navLinks.classList.toggle("show2");
 };
 
+$(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0:{
+        items:1,
+        nav: false
+      },
+      600:{
+        items:2,
+        nav: false
+      },
+      1000:{
+        items:4,
+        nav: false
+      }
+    }
+  });
